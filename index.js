e9a70890181832d201a35cebbe815b2ff2506f66
Ruth Poliakon <r.poliakon@gmail.com>
2019-12-03 20:00:32 -0700

    api project initialization, express installation and imports

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // implement your API here
+const express = require("express");
+const db = require("./data/db");
+
+//////// ENDPOINTS ////////
+
+// POST api/users
+
+// GET api/users
+
+// GET api/users:id
+
+// DELETE api/users/:id
+
+// PUT api/users/:id
